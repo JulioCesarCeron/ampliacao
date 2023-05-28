f7805f7a79c8660ff66937ffb3e3923627f237d5
Julio Cesar Ceron <julio.ceron@mercadolivre.com>
2023-05-28 19:01:29 -0300

    Add contact component

--- components/main/index.tsx
+++ components/main/index.tsx
@@ -1,6 +1,7 @@
 import About from "../pages/about/about"
 import Gallery from "../pages/gallery/gallery"
 import Features from "../pages/features/features"
+import Contact from "../pages/contact/contact"
 import Team from "../pages/team/team"
 
 const Main = () => (
@@ -9,6 +10,7 @@ const Main = () => (
     <Team />
     <Gallery />
     <Features />
+    <Contact />
   </main>
 )
 
---
+++ components/pages/contact/contact.tsx
@@ -0,0 +1,48 @@
+const Contact = () => (
+  <section
+    className="u-align-center u-clearfix u-grey-5 u-section-5"
+    id="sec-72e4"
+  >
+    <div className="u-clearfix u-sheet u-valign-middle u-sheet-1">
+      <h2 className="u-text u-text-default u-text-1">Contact Us</h2>
+      <p className="u-text u-text-2">
+        Sample text. Lorem ipsum dolor sit amet, consectetur adipiscing elit
+        nullam nunc justo sagittis suscipit ultrices.
+      </p>
+      <div className="u-align-left u-expanded-width u-grey-10 u-map u-map-1">
+        <div className="embed-responsive">
+          <iframe
+            className="embed-responsive-item"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3536.4187951224085!2d-48.600980005845585!3d-27.580542860208062!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x952749f57044c8f3%3A0xa4d1697583b22606!2zQW1wbGlBw6fDo28gLSBFc3Bhw6dvIENsw61uaWNv!5e0!3m2!1spt-BR!2sus!4v1685310367669!5m2!1spt-BR!2sus"
+            loading="lazy"
+          />
+        </div>
+      </div>
+      <div className="u-clearfix u-expanded-width u-layout-wrap u-layout-wrap-1">
+        <div className="u-gutter-0 u-layout">
+          <div className="u-layout-row">
+            <div className="u-align-left u-container-style u-layout-cell u-left-cell u-size-30 u-layout-cell-1">
+              <div className="u-container-layout u-valign-top u-container-layout-1">
+                <h4 className="u-text u-text-3">Empire State Building</h4>
+                <p className="u-text u-text-4">
+                  350 5th Ave, New York, NY 10118
+                </p>
+              </div>
+            </div>
+            <div className="u-align-left u-container-style u-layout-cell u-right-cell u-size-30 u-layout-cell-2">
+              <div className="u-container-layout u-container-layout-2">
+                <h4 className="u-text u-text-5">Sample Headline</h4>
+                <p className="u-text u-text-6">
+                  Sample text. Lorem ipsum dolor sit amet, consectetur
+                  adipiscing elit nullam nunc justo sagittis suscipit ultrices.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+)
+
+export default Contact
