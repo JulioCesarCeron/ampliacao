48d97b6d5ec861f6507b38344a14ff2c1e4dc9d4
Julio Cesar Ceron <julio.ceron@mercadolivre.com>
2023-05-28 18:59:15 -0300

    Add gallery component

--- components/main/index.tsx
+++ components/main/index.tsx
@@ -1,10 +1,12 @@
 import About from "../pages/about/about"
+import Gallery from "../pages/gallery/gallery"
 import Team from "../pages/team/team"
 
 const Main = () => (
   <main id="main">
     <About />
     <Team />
+    <Gallery />
   </main>
 )
 
---
+++ components/pages/gallery/gallery.tsx
@@ -0,0 +1,141 @@
+import Image from "next/image"
+import imageGallery from "../../../assets/svg/placeholder-gallery.svg"
+
+const Gallery = () => (
+  <section
+    className="u-align-center u-clearfix u-grey-5 u-section-3"
+    id="sec-ea09"
+  >
+    <div className="u-clearfix u-sheet u-valign-middle-lg u-valign-middle-md u-valign-middle-xl u-sheet-1">
+      <h2 className="u-text u-text-default u-text-1">Sample Headline</h2>
+      <div className="u-expanded-width-lg u-expanded-width-md u-expanded-width-xl u-gallery u-layout-horizontal u-lightbox u-no-transition u-show-text-on-hover u-width-fixed u-gallery-1">
+        <div className="u-gallery-inner">
+          <div className="u-effect-fade u-gallery-item u-gallery-item-1">
+            <div className="u-back-slide">
+              <Image
+                alt="image-1"
+                width={375}
+                height={undefined}
+                className="u-back-image u-back-image-1"
+                src={imageGallery}
+              />
+            </div>
+            <div className="u-over-slide u-shading u-over-slide-1">
+              <h3 className="u-gallery-heading"></h3>
+              <p className="u-gallery-text"></p>
+            </div>
+          </div>
+          <div className="u-effect-fade u-gallery-item u-gallery-item-2">
+            <div className="u-back-slide">
+              <Image
+                alt="image-2"
+                width={375}
+                height={undefined}
+                className="u-back-image u-back-image-2"
+                src={imageGallery}
+              />
+            </div>
+            <div className="u-over-slide u-shading u-over-slide-2">
+              <h3 className="u-gallery-heading"></h3>
+              <p className="u-gallery-text"></p>
+            </div>
+          </div>
+          <div className="u-effect-fade u-gallery-item u-gallery-item-3">
+            <div className="u-back-slide">
+              <Image
+                alt="image-3"
+                width={375}
+                height={undefined}
+                className="u-back-image u-back-image-3"
+                src={imageGallery}
+              />
+            </div>
+            <div className="u-over-slide u-shading u-over-slide-3">
+              <h3 className="u-gallery-heading"></h3>
+              <p className="u-gallery-text"></p>
+            </div>
+          </div>
+          <div className="u-effect-fade u-gallery-item u-gallery-item-4">
+            <div className="u-back-slide">
+              <Image
+                alt="image-4"
+                width={375}
+                height={undefined}
+                className="u-back-image u-back-image-4"
+                src={imageGallery}
+              />
+            </div>
+            <div className="u-over-slide u-shading u-over-slide-4">
+              <h3 className="u-gallery-heading"></h3>
+              <p className="u-gallery-text"></p>
+            </div>
+          </div>
+          <div className="u-effect-fade u-gallery-item u-gallery-item-5">
+            <div className="u-back-slide">
+              <Image
+                alt="image-5"
+                width={375}
+                height={undefined}
+                className="u-back-image u-back-image-5"
+                src={imageGallery}
+              />
+            </div>
+            <div className="u-over-slide u-shading u-over-slide-5">
+              <h3 className="u-gallery-heading"></h3>
+              <p className="u-gallery-text"></p>
+            </div>
+          </div>
+          <div className="u-effect-fade u-gallery-item u-gallery-item-6">
+            <div className="u-back-slide">
+              <Image
+                alt="image-6"
+                width={375}
+                height={undefined}
+                className="u-back-image u-back-image-6"
+                src={imageGallery}
+              />
+            </div>
+            <div className="u-over-slide u-shading u-over-slide-6">
+              <h3 className="u-gallery-heading"></h3>
+              <p className="u-gallery-text"></p>
+            </div>
+          </div>
+        </div>
+        <a
+          className="u-absolute-vcenter u-gallery-nav u-gallery-nav-prev u-grey-70 u-icon-circle u-opacity u-opacity-70 u-spacing-10 u-text-white u-gallery-nav-1"
+          href="#"
+          role="button"
+        >
+          <span aria-hidden="true">
+            <svg viewBox="0 0 451.847 451.847">
+              <path d="M97.141,225.92c0-8.095,3.091-16.192,9.259-22.366L300.689,9.27c12.359-12.359,32.397-12.359,44.751,0c12.354,12.354,12.354,32.388,0,44.748L173.525,225.92l171.903,171.909c12.354,12.354,12.354,32.391,0,44.744c-12.354,12.365-32.386,12.365-44.745,0l-194.29-194.281C100.226,242.115,97.141,234.018,97.141,225.92z" />
+            </svg>
+          </span>
+          <span className="sr-only">
+            <svg viewBox="0 0 451.847 451.847">
+              <path d="M97.141,225.92c0-8.095,3.091-16.192,9.259-22.366L300.689,9.27c12.359-12.359,32.397-12.359,44.751,0c12.354,12.354,12.354,32.388,0,44.748L173.525,225.92l171.903,171.909c12.354,12.354,12.354,32.391,0,44.744c-12.354,12.365-32.386,12.365-44.745,0l-194.29-194.281C100.226,242.115,97.141,234.018,97.141,225.92z" />
+            </svg>
+          </span>
+        </a>
+        <a
+          className="u-absolute-vcenter u-gallery-nav u-gallery-nav-next u-grey-70 u-icon-circle u-opacity u-opacity-70 u-spacing-10 u-text-white u-gallery-nav-2"
+          href="#"
+          role="button"
+        >
+          <span aria-hidden="true">
+            <svg viewBox="0 0 451.846 451.847">
+              <path d="M345.441,248.292L151.154,442.573c-12.359,12.365-32.397,12.365-44.75,0c-12.354-12.354-12.354-32.391,0-44.744L278.318,225.92L106.409,54.017c-12.354-12.359-12.354-32.394,0-44.748c12.354-12.359,32.391-12.359,44.75,0l194.287,194.284c6.177,6.18,9.262,14.271,9.262,22.366C354.708,234.018,351.617,242.115,345.441,248.292z" />
+            </svg>
+          </span>
+          <span className="sr-only">
+            <svg viewBox="0 0 451.846 451.847">
+              <path d="M345.441,248.292L151.154,442.573c-12.359,12.365-32.397,12.365-44.75,0c-12.354-12.354-12.354-32.391,0-44.744L278.318,225.92L106.409,54.017c-12.354-12.359-12.354-32.394,0-44.748c12.354-12.359,32.391-12.359,44.75,0l194.287,194.284c6.177,6.18,9.262,14.271,9.262,22.366C354.708,234.018,351.617,242.115,345.441,248.292z" />
+            </svg>
+          </span>
+        </a>
+      </div>
+    </div>
+  </section>
+)
+
+export default Gallery
